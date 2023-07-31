c425196c913627699963db018bb1dce999dd318b
Scaffolder <scaffolder@backstage.io>
2023-07-31 19:53:22 +0000

    initial commit

---
+++ jhipsterSampleApplicationRun1/src/main/java/com/mycompany/myapp/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package com.backstage.jhipster.security;
+
+import com.backstage.jhipster.config.Constants;
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM));
+    }
+}
---
+++ jhipsterSampleApplicationRun1/src/test/java/com/mycompany/myapp/security/jwt/AuthenticationIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.backstage.jhipster.security.jwt;
+
+import com.backstage.jhipster.config.SecurityConfiguration;
+import com.backstage.jhipster.config.SecurityJwtConfiguration;
+import com.backstage.jhipster.config.WebConfigurer;
+import com.backstage.jhipster.management.SecurityMetersService;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import tech.jhipster.config.JHipsterProperties;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(
+    classes = {
+        JHipsterProperties.class,
+        WebConfigurer.class,
+        SecurityConfiguration.class,
+        SecurityJwtConfiguration.class,
+        SecurityMetersService.class,
+        JwtAuthenticationTestUtils.class,
+    }
+)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface AuthenticationIntegrationTest {
+}
